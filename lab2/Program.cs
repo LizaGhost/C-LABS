4c3d03ce41a31355f76d5e7a7b6364639e9ccd5a
LizaGhost <160327258+LizaGhost@users.noreply.github.com>
2026-04-14 16:17:04 +0400

    Add files via upload

---
+++ lab2/Program.cs
@@ -0,0 +1,109 @@
+﻿using lab1;
+using lab2;
+using Yureva1;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop(); //создаётся экземпляр класса Shop, заполняется список товаров
+
+            shop.Items.Add(new Item("Tion", "IQ200", 30, InstallationEnum.OUTDOOR, 46, 17, 17200));
+            shop.Items.Add(new Item("Philips", "AC2729/10", 85, InstallationEnum.OUTDOOR, 40, 35, 30890));
+            shop.Items.Add(new Item("Ballu", "AP-130", 40, InstallationEnum.OUTDOOR, 55, 48, 11733));
+            shop.Items.Add(new Item("VITEK", "VT-8558", 30, InstallationEnum.OUTDOOR, 55, 25, 6906));
+            shop.Items.Add(new Item("Dyson", "TP00-1kz", 40, InstallationEnum.DESKTOP, 65, 56, 30265));
+            shop.Items.Add(new Item("Philips", "AC1715/10", 78, InstallationEnum.DESKTOP, 50, 27, 22206));
+            shop.Items.Add(new Item("Brayer", "4930", 18, InstallationEnum.DESKTOP, 55, 25, 4878));
+            shop.Items.Add(new Item("Harper", "KC-41", 30, InstallationEnum.OUTDOOR, 30, 45, 8879));
+            shop.Items.Add(new Item("Atmeex", "A7 Flow", 50, InstallationEnum.WALL_MOUNTED, 43, 1585, 65258));
+            shop.Items.Add(new Item("Ballu", "asp", 30, InstallationEnum.WALL_MOUNTED, 36, 615, 16191));
+            shop.Items.Add(new Item("Ballu", "asp-80", 30, InstallationEnum.WALL_MOUNTED, 24, 615, 16711));
+            shop.Items.Add(new Item("Tion", "Lite", 20, InstallationEnum.WALL_MOUNTED, 48, 900, 33700));
+            shop.Items.Add(new Item("Tion", "O2 Base", 30, InstallationEnum.WALL_MOUNTED, 52, 1450, 40650));
+            shop.Items.Add(new Item("Electrolux", "EAP-2050D", 50, InstallationEnum.DESKTOP, 21, 36, 18171));
+            shop.Items.Add(new Item("Harper", "KC-31", 30, InstallationEnum.OUTDOOR, 30, 35, 7440));
+
+            //Выводы товаров + сортировки
+
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Все товары");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            Shop.CostSorter costSorter= new Shop.CostSorter();
+            costSorter.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка по цене:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            Shop.PowerSorter powerSorter = new Shop.PowerSorter();
+            powerSorter.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка по мощности:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            Shop.MaxNoiceLevel maxNoiceLevel = new Shop.MaxNoiceLevel();
+            maxNoiceLevel.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка по максимальному значению шума:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            Shop.Installation installation = new Shop.Installation();
+            installation.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка по типу установки:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            Shop.AreaCoverage areaCoverage = new Shop.AreaCoverage();
+            areaCoverage.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка по охвату площади:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            Shop.Model model = new Shop.Model();
+            model.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка по модели:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            Shop.Brand brand = new Shop.Brand();
+            brand.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка по бренду:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            // Сортировка и вывод товаров с использованием алгоритмов сортировок
+
+            Shop.BubbleSorter bubbleSorter = new Shop.BubbleSorter();
+            bubbleSorter.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка пузырьком по цене:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            Shop.InsertionSorter insertionSorter = new Shop.InsertionSorter();
+            insertionSorter.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка вставками по цене:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+
+            Shop.CocktailSorter cocktailSorter = new Shop.CocktailSorter();
+            cocktailSorter.Sort(shop.Items);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Сортировка перемешиванием по цене:");
+            Console.WriteLine("---------------------------------------------------------");
+            shop.ShowItems();
+        }
+    }
+}
---
+++ lab2/Shop.cs
@@ -0,0 +1,199 @@
+﻿using Yureva1;
+using lab1;
+
+namespace lab2
+{
+    public class Shop
+    {
+        private List<Item> items = new List<Item>(); //агрегирование (процесс, при котором один объект содержит ссылку на другой объект. Shop содержит ссылку на список объектов Item)
+
+        public List<Item> Items // свойство для доступа к списку товаров
+        {
+            get { return items; }
+            set { items = value; }
+        }
+
+        public void ShowItems() // метод для отображения всех товаров в списке
+        {
+            Console.WriteLine("Brand      Model     Area   Installation MaxNoice  Power  Cost");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("---------------------------------------------------------");
+        }
+
+        public abstract class Sorter // абстрактный класс сортировщик (использование только в качестве базового класса для других классов)
+        {
+            //0: равны, -1: X меньше Y, 1: X болше Y
+            public abstract int Compare(Item X, Item Y); // абстрактный метод (метод без реализации) для сравнения двух объектов
+            public virtual void Sort(List<Item> list) // виртуальный метод сортировки списка
+            {
+                list.Sort(Compare);
+            }
+        }
+
+        // описание классов-сортировщиков, наследуемых от класса Sorter
+
+        public class CostSorter : Sorter
+        {
+            public override int Compare(Item X, Item Y)
+            {
+                return X.Cost.CompareTo(Y.Cost);
+            }
+        }
+
+        public class PowerSorter : Sorter
+        {
+            public override int Compare(Item X, Item Y)
+            {
+                return X.Power.CompareTo(Y.Power);
+            }
+        }
+
+        public class MaxNoiceLevel : Sorter
+        {
+            public override int Compare(Item X, Item Y)
+            {
+                return X.MaxNoiceLevel.CompareTo(Y.MaxNoiceLevel);
+            }
+        }
+
+        public class Installation : Sorter
+        {
+            public override int Compare(Item X, Item Y)
+            {
+                return X.Installation.CompareTo(Y.Installation);
+            }
+        }
+
+        public class AreaCoverage : Sorter
+        {
+            public override int Compare(Item X, Item Y)
+            {
+                return X.AreaCoverage.CompareTo(Y.AreaCoverage);
+            }
+        }
+
+        public class Model : Sorter
+        {
+            public override int Compare(Item X, Item Y)
+            {
+                return X.Model.CompareTo(Y.Model);
+            }
+        }
+
+        public class Brand : Sorter
+        {
+            public override int Compare(Item X, Item Y)
+            {
+                return X.Brand.CompareTo(Y.Brand);
+            }
+        }
+
+        // Сортировка пузырьком
+        public class BubbleSorter : Sorter
+        {
+            public override int Compare(Item x, Item y)
+            {
+                // Сравнение по цене, можно изменить критерий
+                return x.Cost.CompareTo(y.Cost);
+            }
+
+            public override void Sort(List<Item> list)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    for (int j = 0; j < list.Count - i - 1; j++)
+                    {
+                        if (Compare(list[j], list[j + 1]) > 0)
+                        {
+                            var temp = list[j];
+                            list[j] = list[j + 1];
+                            list[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Сортировка вставками
+        public class InsertionSorter : Sorter
+        {
+            public override int Compare(Item x, Item y)
+            {
+                // Сравнение по цене, можно изменить критерий
+                return x.Cost.CompareTo(y.Cost);
+            }
+
+            public override void Sort(List<Item> list)
+            {
+                for (int i = 1; i < list.Count; i++)
+                {
+                    var key = list[i];
+                    int j = i - 1;
+
+                    while (j >= 0 && Compare(list[j], key) > 0)
+                    {
+                        list[j + 1] = list[j];
+                        j--;
+                    }
+                    list[j + 1] = key;
+                }
+            }
+        }
+
+        // Сортировка перемешиванием
+        public class CocktailSorter : Sorter
+        {
+            public override int Compare(Item x, Item y)
+            {
+                // Сравнение по цене, можно изменить критерий
+                return x.Cost.CompareTo(y.Cost);
+            }
+
+            public override void Sort(List<Item> list)
+            {
+                bool swapped = true;
+                int start = 0;
+                int end = list.Count;
+
+                while (swapped)
+                {
+                    swapped = false;
+
+                    for (int i = start; i < end - 1; ++i)
+                    {
+                        if (Compare(list[i], list[i + 1]) > 0)
+                        {
+                            var temp = list[i];
+                            list[i] = list[i + 1];
+                            list[i + 1] = temp;
+                            swapped = true;
+                        }
+                    }
+
+                    if (!swapped)
+                        break;
+
+                    swapped = false;
+                    end--;
+
+                    for (int i = end - 1; i >= start; i--)
+                    {
+                        if (Compare(list[i], list[i + 1]) > 0)
+                        {
+                            var temp = list[i];
+                            list[i] = list[i + 1];
+                            list[i + 1] = temp;
+                            swapped = true;
+                        }
+                    }
+                    start++;
+                }
+            }
+
+        }
+
+    }
+}
