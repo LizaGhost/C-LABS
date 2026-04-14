fd700a91090bcf8aea2e039018fdf5b9df39ea86
LizaGhost <160327258+LizaGhost@users.noreply.github.com>
2026-04-14 16:18:59 +0400

    Add files via upload

---
+++ lab3/Program.cs
@@ -0,0 +1,73 @@
+﻿using lab1;
+using lab3;
+using System.Threading;
+using Yureva1;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop(); //создаётся экземпляр класса Shop, заполняется список товаров
+
+            // Подписываемся на событие завершения сортировки
+            shop.FinishedEvent += Shop_SortingFinished;
+
+            shop.Items.Add(new Item("Tion", "IQ200", 30, InstallationEnum.OUTDOOR, 46, 17, 17200));
+            shop.Items.Add(new Item("Philips", "AC2729/10", 85, InstallationEnum.OUTDOOR, 40, 35, 30890));
+            shop.Items.Add(new Item("Ballu", "AP-130", 40, InstallationEnum.OUTDOOR, 55, 48, 11733));
+            shop.Items.Add(new Item("VITEK", "VT-8558", 30, InstallationEnum.OUTDOOR, 55, 25, 6906));
+            shop.Items.Add(new Item("Dyson", "TP00-1kz", 40, InstallationEnum.DESKTOP, 65, 56, 30265));
+            shop.Items.Add(new Item("Philips", "AC1715/10", 78, InstallationEnum.DESKTOP, 50, 27, 22206));
+            shop.Items.Add(new Item("Brayer", "4930", 18, InstallationEnum.DESKTOP, 55, 25, 4878));
+            shop.Items.Add(new Item("Harper", "KC-41", 30, InstallationEnum.OUTDOOR, 30, 45, 8879));
+            shop.Items.Add(new Item("Atmeex", "A7 Flow", 50, InstallationEnum.WALL_MOUNTED, 43, 1585, 65258));
+            shop.Items.Add(new Item("Ballu", "asp", 30, InstallationEnum.WALL_MOUNTED, 36, 615, 16191));
+            shop.Items.Add(new Item("Ballu", "asp-80", 30, InstallationEnum.WALL_MOUNTED, 24, 615, 16711));
+            shop.Items.Add(new Item("Tion", "Lite", 20, InstallationEnum.WALL_MOUNTED, 48, 900, 33700));
+            shop.Items.Add(new Item("Tion", "O2 Base", 30, InstallationEnum.WALL_MOUNTED, 52, 1450, 40650));
+            shop.Items.Add(new Item("Electrolux", "EAP-2050D", 50, InstallationEnum.DESKTOP, 21, 36, 18171));
+            shop.Items.Add(new Item("Harper", "KC-31", 30, InstallationEnum.OUTDOOR, 30, 35, 7440));
+
+            // вывод оригинального списка
+            Console.WriteLine("Обычный список:");
+            shop.ShowItems();
+
+            Console.WriteLine("Обычная сортировка по цене:"); // обычный метод
+            shop.Sort(Shop.CompareByCost);
+            //shop.ShowItems();
+
+            Console.WriteLine("\nОбычная сортировка по мощности с указателем"); // обычный метод плюс указатель на него
+            Func<Item, Item, int> compareByPower = Shop.CompareByPower;
+            shop.Sort(compareByPower.Invoke);
+            //shop.ShowItems();
+
+            // анонимный метод
+            Console.WriteLine("\nАнонимная сортировка по максимальному значению шума:");
+            shop.Sort(delegate (Item a, Item b) { return a.MaxNoiceLevel - b.MaxNoiceLevel; });
+            //shop.ShowItems();
+
+            // лямбда-выражение
+            Console.WriteLine("\nЛямбда сортировка по макс. значению шума (Descending):");
+            shop.Sort((a, b) => a.MaxNoiceLevel - b.MaxNoiceLevel, true);
+            //shop.ShowItems();
+
+            // Вызываем асинхронный метод сортировки
+            Console.WriteLine("\nАсинхр. сортировка по площади:");
+            shop.SortAsync(Shop.CompareByArea);
+            for (int i = 0; i < 10; i++) // работа, выполняемая параллельно с сортировкой
+            {
+                Console.WriteLine($"Работаю... {i + 1}");
+                Thread.Sleep(200); // Задержка для демонстрации параллельной работы
+            }
+
+        }
+        private static void Shop_SortingFinished(Shop sender)
+        {
+            // Выводим отсортированную таблицу после завершения сортировки
+            Console.WriteLine("Сортировка завершена:");
+            sender.ShowItems();
+        }
+    }
+}
---
+++ lab3/Shop.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yureva1;
+using lab1;
+
+namespace lab3
+{
+    public class Shop
+    {
+        private List<Item> items = new List<Item>(); //агрегирование (процесс, при котором один объект содержит ссылку на другой объект. Shop содержит ссылку на список объектов Item)
+
+        public List<Item> Items // свойство для доступа к списку товаров
+        {
+            get { return items; }
+            set { items = value; }
+        }
+
+        public delegate int Compare(Item a, Item b);
+
+        public delegate void Finished(Shop sender); //делегат для функции обратного вызова
+
+        public event Finished FinishedEvent; //событие
+
+        public void Sort(Compare compare, bool Descending = false) // descending - по убыванию
+        {
+
+            Console.WriteLine("Сортировка началась");
+            items.Sort((a, b) =>
+            {
+                int result = compare(a, b);
+                return Descending ? -result : result;
+            });
+
+            // Симуляция длительного процесса
+            Thread.Sleep(500);
+
+            // Триггер на FinishedEvent
+            FinishedEvent?.Invoke(this);
+        }
+
+        public void SortAsync(Compare compare, bool Descending = false)
+        {
+            Task.Run(() =>
+            {
+                Sort(compare, Descending);
+            });
+        }
+
+        public void ShowItems()
+        {
+            Console.WriteLine("Brand      Model     Area   Installation MaxNoice  Power  Cost");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("---------------------------------------------------------");
+        }
+
+        // Методы-сравнители для простой сортировки по каждому полю
+        public static int CompareByCost(Item a, Item b)
+        {
+            return a.Cost.CompareTo(b.Cost);
+        }
+        public static int CompareByPower(Item a, Item b)
+        {
+            return a.Power.CompareTo(b.Power);
+        }
+
+        public static int CompareByMaxNoice(Item a, Item b)
+        {
+            return a.MaxNoiceLevel.CompareTo(b.MaxNoiceLevel);
+        }
+
+        public static int CompareByInstallation(Item a, Item b)
+        {
+            return a.Installation.CompareTo(b.Installation);
+        }
+
+        public static int CompareByArea(Item a, Item b)
+        {
+            return a.AreaCoverage.CompareTo(b.AreaCoverage);
+        }
+
+        public static int CompareByModel(Item a, Item b)
+        {
+            return a.Model.CompareTo(b.Model);
+        }
+
+        public static int CompareByBrand(Item a, Item b)
+        {
+            return a.Brand.CompareTo(b.Brand);
+        }
+    }
+}
